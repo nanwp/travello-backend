b66615cd7c9268f3e534a21231605fb46fa49349
nanda <nandawijaya@apps.ipb.ac.id>
2023-05-16 11:37:25 +0700

    return user not verified

--- handler/user_handler.go
+++ handler/user_handler.go
@@ -206,48 +206,54 @@ func (h *userHandler) Login(c *gin.Context) {
 		return
 	}
 
-	expTime := time.Now().Add(time.Hour * 24)
-	claims := &config.JWTClaim{
-		UserID: userLogin.ID,
-		RegisteredClaims: jwt.RegisteredClaims{
-			Issuer:    "trevello",
-			ExpiresAt: jwt.NewNumericDate(expTime),
-		},
-	}
+	if userLogin.Verified {
+		expTime := time.Now().Add(time.Hour * 24)
+		claims := &config.JWTClaim{
+			UserID: userLogin.ID,
+			RegisteredClaims: jwt.RegisteredClaims{
+				Issuer:    "trevello",
+				ExpiresAt: jwt.NewNumericDate(expTime),
+			},
+		}
 
-	tokenAlgo := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+		tokenAlgo := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 
-	token, err := tokenAlgo.SignedString(config.JWT_KEY)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"status":  "INTERNAL_SERVER_ERROR",
-			"message": err.Error(),
+		token, err := tokenAlgo.SignedString(config.JWT_KEY)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"status":  "INTERNAL_SERVER_ERROR",
+				"message": err.Error(),
+			})
+			helper.ResponseOutput(c, http.StatusInternalServerError, "INTERNAL_SERVER_ERROR", err, nil)
+			return
+		}
+
+		c.SetSameSite(http.SameSiteLaxMode)
+
+		http.SetCookie(c.Writer, &http.Cookie{
+			Name:     "token",
+			Value:    token,
+			MaxAge:   3600 * 24 * 30,
+			Path:     "/",
+			HttpOnly: true,
 		})
-		helper.ResponseOutput(c, http.StatusInternalServerError, "INTERNAL_SERVER_ERROR", err, nil)
+
+		type resp struct {
+			Name  string `json:"name"`
+			Email string `json:"email"`
+			Token string `json:"token"`
+		}
+
+		datRes := resp{
+			Name:  userLogin.Name,
+			Email: userLogin.Email,
+			Token: token,
+		}
+
+		helper.ResponseOutput(c, http.StatusOK, "OK", "sucess login", datRes)
 		return
 	}
 
-	c.SetSameSite(http.SameSiteLaxMode)
+	helper.ResponseOutput(c, http.StatusBadRequest, "BAD_REQUEST", "user not verified, please check your email", nil)
 
-	http.SetCookie(c.Writer, &http.Cookie{
-		Name:     "token",
-		Value:    token,
-		MaxAge:   3600 * 24 * 30,
-		Path:     "/",
-		HttpOnly: true,
-	})
-
-	type resp struct {
-		Name  string `json:"name"`
-		Email string `json:"email"`
-		Token string `json:"token"`
-	}
-
-	datRes := resp{
-		Name:  userLogin.Name,
-		Email: userLogin.Email,
-		Token: token,
-	}
-
-	helper.ResponseOutput(c, http.StatusOK, "OK", "sucess login", datRes)
 }
