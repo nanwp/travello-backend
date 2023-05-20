b93b4aebe619a6a67873ff025fabf2d5710d96dd
nanda <nandawijaya@apps.ipb.ac.id>
2023-05-20 18:08:13 +0700

    array image

--- models/destinations/entitiy.go
+++ models/destinations/entitiy.go
@@ -1,25 +1,25 @@
 package destinations
 
 type Destination struct {
-	ID          string  `json:"_id"`
-	Nama        string  `json:"name" binding:"required"`
-	Description string  `json:"description" binding:"required"`
-	Location    string  `json:"location" binding:"required"`
-	Category    string  `json:"category"`
-	Image       string  `json:"image"`
-	Rating      float32 `json:"rating"`
-	CreatedAt   string  `json:"created_at"`
-	UpdatedAt   string  `json:"updated_at"`
+	ID          string   `json:"_id"`
+	Nama        string   `json:"name" binding:"required"`
+	Description string   `json:"description" binding:"required"`
+	Location    string   `json:"location" binding:"required"`
+	Category    string   `json:"category"`
+	Image       []string `json:"image"`
+	Rating      float32  `json:"rating"`
+	CreatedAt   string   `json:"created_at"`
+	UpdatedAt   string   `json:"updated_at"`
 }
 
 type DestinationResponse struct {
-	ID          string  `json:"id,omitempty"`
-	Nama        string  `json:"name,omitempty"`
-	Description string  `json:"description,omitempty"`
-	Location    string  `json:"location,omitempty"`
-	Category    string  `json:"category,omitempty"`
-	Image       string  `json:"image,omitempty"`
-	Rating      float32 `json:"rating"`
-	CreatedAt   string  `json:"created_at,omitempty"`
-	UpdatedAt   string  `json:"updated_at,omitempty"`
+	ID          string   `json:"id,omitempty"`
+	Nama        string   `json:"name,omitempty"`
+	Description string   `json:"description,omitempty"`
+	Location    string   `json:"location,omitempty"`
+	Category    string   `json:"category,omitempty"`
+	Image       []string `json:"image,omitempty"`
+	Rating      float32  `json:"rating"`
+	CreatedAt   string   `json:"created_at,omitempty"`
+	UpdatedAt   string   `json:"updated_at,omitempty"`
 }
